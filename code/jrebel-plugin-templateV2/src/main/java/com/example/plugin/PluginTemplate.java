edfb11313ad110cc1ce5418c9acf8fbac2447940
doudousha <dou_dou_sha@sina.com>
2024-05-18 21:47:35 +0800

    1. websocket 替换为http 2. 弃用‘org.java-websocket’，改为okhttp3 3. 增加日志

--- code/jrebel-plugin-templateV2/src/main/java/com/example/plugin/PluginTemplate.java
+++ code/jrebel-plugin-templateV2/src/main/java/com/example/plugin/PluginTemplate.java
@@ -1,5 +1,6 @@
 package com.example.plugin;
 
+import okhttp3.*;
 import org.zeroturnaround.javarebel.ClassResourceSource;
 import org.zeroturnaround.javarebel.Plugin;
 import org.zeroturnaround.javarebel.*;
@@ -7,12 +8,17 @@ import org.zeroturnaround.javarebel.*;
 import java.net.URISyntaxException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /*
 注意: 使用maven assembly 打包，而不是package打包
 
 -Drebel.plugins=D:\\plugin-template-1.0.0.jar -Drebel.log.stdout=true -Drebel.myq.timeout=3600  -Drebel.myq.wsUrl=ws://127.0.0.1:8780
 
+-Drebel.plugins=D:\\plugin-template-1.0.0.jar -Drebel.jrebel-hooks-plugin=true -Drebel.log.stdout=true  -Drebel.myq.message.url=http://192.168.10.116/pub?id=123
+-Drebel.plugins=D:/code/jrebel-reload/code/jrebel-plugin-templateV2/target/plugin-template-1.0.0-jar-with-dependencies.jar -Drebel.log.stdout=true  -Drebel.myq.message.url=http://192.168.10.116/pub?id=123
+
+-Drebel.plugins=D:/code/jrebel-reload/code/jrebel-plugin-templateV2/target/plugin-template-1.0.0-jar-with-dependencies.jar -Drebel.log.stdout=true  -Drebel.myq.message.url=http://192.168.10.116/pub?id=123 -Drebel.xml=D:/code/myq-desgin-ktor/code/myq/design-start/src/main/resources/rebel.xml
 参考：
 调用项目的classLoad: https://github.com/atsu85/jrebel-hooks-plugin
 jrebel参数配置：https://manuals.jrebel.com/jrebel/misc/agentsettings.html
@@ -20,17 +26,18 @@ jrebel参数配置：https://manuals.jrebel.com/jrebel/misc/agentsettings.html
 
  */
 public class PluginTemplate implements Plugin {
-  private  WsHandler ws;
-  private int timeout = 3000;
-  private String wsUrl = "";
+  private  OkHttpClient okHttpClient;
+  private String url;
 
   public PluginTemplate() {
 
   }
 
-  private void registryEvent(){
+  private void registryEvent() {
     // 创建ClassEventListener对象
     Reloader reloader = ReloaderFactory.getInstance();
+
+
     reloader.addClassLoadListener(new ClassEventListener() {
       public void onClassEvent(int eventType, Class klass) {
         try {
@@ -40,6 +47,7 @@ public class PluginTemplate implements Plugin {
 
         }
       }
+
       @Override
       public int priority() {
         return 0;
@@ -52,9 +60,18 @@ public class PluginTemplate implements Plugin {
           log("addClassReloadListener");
           log("重新加载了文件:" + klass.getName() + ",eventType:" + eventType);
 
-          ws.sendMsg("重新加载了文件");
+          // okhtt3 发送消息
+          String json = "{\"action\":\"classes.reload.success\"}";
+          RequestBody body = RequestBody.create(MediaType.parse("application/json"),json);
+          Request request = (new Request.Builder())
+                  .url(url)
+                  .post(body)
+                  .addHeader("Content-Type", "application/json")
+                  .build();
+          Response resp = okHttpClient.newCall(request).execute();
+          log("通知::" + resp.code() +",");
         } catch (final Exception e) {
-
+          log("异常："+e.getMessage());
         }
       }
 
@@ -66,28 +83,20 @@ public class PluginTemplate implements Plugin {
     });
   }
 
+
+
   @Override
   public void preinit() {
     log("PluginTemplate preinit 初始化=======================");
-
-    registryEvent();
-
-    String timeoutStr = System.getProperty("rebel.myq.timeout");
-    String wsUrlStr = System.getProperty("rebel.myq.wsUrl");
-    ws://127.0.0.1:8780
-
-
-    log("缓冲执行时间: Drebel.myq.timeout=" + timeoutStr);
-    if (timeoutStr != null && timeoutStr.trim().length() > 0) {
-      this.timeout = Integer.parseInt(timeoutStr);
-    }
-    log("同步页面服务: Drebel.myq.wsUrl=" + wsUrlStr);
-    if (wsUrlStr != null && wsUrlStr.trim().length() > 0) {
-      this.wsUrl = wsUrlStr;
-    }
+    this.url = System.getProperty("rebel.myq.message.url");
+    log("同步页面服务: rebel.myq.message.url=" + this.url);
 
     try {
-      this.ws = WsHandler.init( this.wsUrl,  this.timeout);
+      this.okHttpClient = (new OkHttpClient()).newBuilder()
+              .readTimeout(3, TimeUnit.SECONDS)
+              .build();
+
+      registryEvent();
       log("ws初始化成功");
     } catch (Exception e) {
       log("连接异常" + e);
