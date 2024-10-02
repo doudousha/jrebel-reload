ccaa6f6f4d85b96867d019a82711b904b67c1812
doudousha <dou_dou_sha@sina.com>
2024-10-02 10:19:39 +0800

    1. 控制日志是否输出配置(rebel.myq.showLog)

--- code/jrebel-plugin-templateV2/src/main/java/com/example/plugin/PluginTemplate.java
+++ code/jrebel-plugin-templateV2/src/main/java/com/example/plugin/PluginTemplate.java
@@ -31,162 +31,167 @@ jrebel参数配置：https://manuals.jrebel.com/jrebel/misc/agentsettings.html
 
  */
 public class PluginTemplate implements Plugin {
-  private  OkHttpClient okHttpClient;
-  private String url;
-  private final static AtomicBoolean locked = new AtomicBoolean(false);
+    private OkHttpClient okHttpClient;
+    private String url;
 
-  private final  AtomicReference<List<TimerTask>> tasks = new AtomicReference<>();
+    /**
+     * 显示日志
+     */
+    private boolean showLog = false;
+    private final static AtomicBoolean locked = new AtomicBoolean(false);
 
-  private final  AtomicReference<TimerTask> taskAtomic = new AtomicReference<>();
-  private static Timer timer = new Timer();
+    private final AtomicReference<List<TimerTask>> tasks = new AtomicReference<>();
 
-  public PluginTemplate() {
-    tasks.set(new ArrayList<TimerTask>());
-  }
+    private final AtomicReference<TimerTask> taskAtomic = new AtomicReference<>();
+    private static Timer timer = new Timer();
 
-  private void sendMsg(){
+    public PluginTemplate() {
+        tasks.set(new ArrayList<TimerTask>());
+    }
 
-  }
+    private void sendMsg() {
 
-  private void registryEvent() {
-    // 创建ClassEventListener对象
-    Reloader reloader = ReloaderFactory.getInstance();
+    }
 
-    reloader.addClassLoadListener(new ClassEventListener() {
-      public void onClassEvent(int eventType, Class klass) {
-        try {
-          log("addClassLoadListener");
-          log("加载了文件:" + klass.getName() + ",eventType:" + eventType);
-        } catch (final Exception e) {
+    private void registryEvent() {
+        // 创建ClassEventListener对象
+        Reloader reloader = ReloaderFactory.getInstance();
+        reloader.addClassLoadListener(new ClassEventListener() {
+            public void onClassEvent(int eventType, Class klass) {
+                try {
+                    log("addClassLoadListener");
+                    log("加载了文件:" + klass.getName() + ",eventType:" + eventType);
+                } catch (final Exception e) {
 
-        }
-      }
-
-      @Override
-      public int priority() {
-        return 0;
-      }
-    });
-
-    reloader.addClassReloadListener(new ClassEventListener() {
-      public void onClassEvent(int eventType, @SuppressWarnings("rawtypes") Class klass) {
-        log("onClassEvent begin");
-        TimerTask task = new TimerTask() {
-          @Override
-          public void run() {
-            try {
-              try {
-                // okhtt3 发送消息
-                String json = "{\"action\":\"classes.reload.success\"}";
-                RequestBody body = RequestBody.create(MediaType.parse("application/json"),json);
-                Request request = (new Request.Builder())
-                        .url(url)
-                        .post(body)
-                        .build();
-                Response resp = okHttpClient.newCall(request).execute();
-                log("通知更新代码:"+resp.code());
-                //log("通知-"+url+": " + resp.code() +",");
-              } catch (final Exception e) {
-                log("异常："+e.getMessage());
-              }finally {
-                tasks.get().clear();
-              }
-
-            } catch (final Exception e) {
-              log("异常：" + e.getMessage());
+                }
             }
-          }
-        };
 
-        for (int i = 0; i < tasks.get().size(); i++) {
-          boolean m = tasks.get().get(i).cancel();
+            @Override
+            public int priority() {
+                return 0;
+            }
+        });
+
+        reloader.addClassReloadListener(new ClassEventListener() {
+            public void onClassEvent(int eventType, @SuppressWarnings("rawtypes") Class klass) {
+                log("onClassEvent begin");
+                TimerTask task = new TimerTask() {
+                    @Override
+                    public void run() {
+                        try {
+                            try {
+                                // okhtt3 发送消息
+                                String json = "{\"action\":\"classes.reload.success\"}";
+                                RequestBody body = RequestBody.create(MediaType.parse("application/json"), json);
+                                Request request = (new Request.Builder())
+                                        .url(url)
+                                        .post(body)
+                                        .build();
+                                Response resp = okHttpClient.newCall(request).execute();
+                                log("通知更新代码:" + resp.code());
+                                //log("通知-"+url+": " + resp.code() +",");
+                            } catch (final Exception e) {
+                                log("异常：" + e.getMessage());
+                            } finally {
+                                tasks.get().clear();
+                            }
+
+                        } catch (final Exception e) {
+                            log("异常：" + e.getMessage());
+                        }
+                    }
+                };
+
+                for (int i = 0; i < tasks.get().size(); i++) {
+                    boolean m = tasks.get().get(i).cancel();
+                }
+
+                // 600毫秒后执行任务
+                timer.schedule(task, 600);
+                tasks.get().add(task);
+
+                log("重新加载了文件:" + klass.getName() + ",eventType:" + eventType + "");
+            }
+
+            @Override
+            public int priority() {
+                return 0;
+            }
+
+        });
+    }
+
+
+    @Override
+    public void preinit() {
+        this.url = System.getProperty("rebel.myq.wsUrl");
+        this.showLog = System.getProperty("rebel.myq.showLog") == "true";
+
+        log("PluginTemplate preinit 初始化=======================");
+        log("同步页面服务: rebel.myq.message.url=" + this.url);
+
+        try {
+            this.okHttpClient = (new OkHttpClient()).newBuilder()
+                    .readTimeout(3, TimeUnit.SECONDS)
+                    .build();
+
+            registryEvent();
+            log("ws初始化成功:" + url);
+        } catch (Exception e) {
+            log("连接异常" + e);
         }
 
-        // 600毫秒后执行任务
-        timer.schedule(task, 600);
-        tasks.get().add(task);
-
-        log("重新加载了文件:" + klass.getName() + ",eventType:" + eventType+"");
-      }
-
-      @Override
-      public int priority() {
-        return 0;
-      }
-
-    });
-  }
-
-
-
-  @Override
-  public void preinit() {
-    log("PluginTemplate preinit 初始化=======================");
-    this.url = System.getProperty("rebel.myq.message.url");
-    log("同步页面服务: rebel.myq.message.url=" + this.url);
-
-    try {
-      this.okHttpClient = (new OkHttpClient()).newBuilder()
-              .readTimeout(3, TimeUnit.SECONDS)
-              .build();
-
-      registryEvent();
-      log("ws初始化成功");
-    } catch (Exception e) {
-      log("连接异常" + e);
     }
 
-  }
-
-  public void log(String str){
-    try {
-
-      LoggerFactory.getInstance().echo(str);
-    } catch (Exception e) {
-      LoggerFactory.getInstance().echo("error===========:" +str);
+    public void log(String str) {
+        try {
+            if (showLog) {
+                LoggerFactory.getInstance().echo(str);
+            }
+        } catch (Exception e) {
+            LoggerFactory.getInstance().echo("error===========:" + str);
+        }
     }
-  }
 
 
+    @Override
+    public boolean checkDependencies(ClassLoader cl, ClassResourceSource crs) {
+        // check if plugin should be enabled in classloader cl
+        return true;
+    }
 
-  @Override
-  public boolean checkDependencies(ClassLoader cl, ClassResourceSource crs) {
-    // check if plugin should be enabled in classloader cl
-    return true;
-  }
+    @Override
+    public String getId() {
+        return "PluginTemplate";
+    }
 
-  @Override
-  public String getId() {
-    return "PluginTemplate";
-  }
+    @Override
+    public String getName() {
+        return "PluginTemplate";
+    }
 
-  @Override
-  public String getName() {
-    return "PluginTemplate";
-  }
+    @Override
+    public String getDescription() {
+        return "PluginTemplate";
+    }
 
-  @Override
-  public String getDescription() {
-    return "PluginTemplate";
-  }
+    @Override
+    public String getAuthor() {
+        return "PluginTemplate";
+    }
 
-  @Override
-  public String getAuthor() {
-    return "PluginTemplate";
-  }
+    @Override
+    public String getWebsite() {
+        return "PluginTemplate";
+    }
 
-  @Override
-  public String getWebsite() {
-    return "PluginTemplate";
-  }
+    @Override
+    public String getSupportedVersions() {
+        return null;
+    }
 
-  @Override
-  public String getSupportedVersions() {
-    return null;
-  }
-
-  @Override
-  public String getTestedVersions() {
-    return null;
-  }
+    @Override
+    public String getTestedVersions() {
+        return null;
+    }
 }
